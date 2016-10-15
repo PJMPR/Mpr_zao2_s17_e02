65052ca2bf428281bc020df1e32b9b50dcff82c9
s14499 <s14499@github.com>
2016-10-15 13:14:53 +0200

    raight solution v.0.1

--- src/main/java/gitDemo/gitdemo/Ostateczne_rozwiazanie_kwesti_ludzkosci.java
+++ src/main/java/gitDemo/gitdemo/Ostateczne_rozwiazanie_kwesti_ludzkosci.java
@@ -3,6 +3,6 @@ package gitDemo.gitdemo;
 public class Ostateczne_rozwiazanie_kwesti_ludzkosci {
 	boolean are_humans_ok = true;
 	if (are_humans_ok = false){
-		kill_humans();
+		kill_humans(); // Not working, damn it worked in c++
 	}
 }
